85c6a47c93e4dad3edcd40552ad1cc544c68c7d0
lijiajia <lijiajia_1010@126.com>
2015-04-21 02:52:40 -0700

    first commit

---
+++ test_mapreduce/src/main/java/test_mapreduce/App.java
@@ -0,0 +1,13 @@
+package test_mapreduce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
